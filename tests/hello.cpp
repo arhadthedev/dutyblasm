ff2704e00eaf2d5171cc42a113f17d22cb6ff46f
Oleg Iarygin <oleg@arhadthedev.net>
2022-02-16 23:24:26 +0300

    Add a CI aware test project

---
+++ tests/hello.cpp
@@ -0,0 +1,13 @@
+/** hello.cpp - a suit to test if GTest works
+ *
+ * Copyright (c) 2022 Oleg Iarygin <oleg@arhadthedev.net>
+ *
+ * Distributed under the MIT software license; see the accompanying
+ * file LICENSE.txt or <https://www.opensource.org/licenses/mit-license.php>.
+ */
+
+#include <gtest/gtest.h>
+
+TEST(harness, hello) {
+	EXPECT_TRUE(true);
+}
